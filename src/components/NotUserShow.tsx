294df1679c8aa8fee33cf37cc27396a2ec5b6e1e
Adrián Fonseca Vega <afonsecav88@gmail.com>
2023-12-10 15:24:01 -0300

    fixed title of component NotUserShow

--- src/components/NotUserShow.tsx
+++ src/components/NotUserShow.tsx
@@ -2,8 +2,8 @@ export const NotUserShow = () => {
   return (
     <>
       <div className="alert alert-success" role="alert">
-        <h4 className="alert-heading">No hay usarios para mostrar!</h4>
-        <p>Actualmente no hay usuarios para mostrar</p>
+        <h4 className="alert-heading">No hay usarios para mostrar !</h4>
+        <p>Actualmente no hay usuarios para mostrar.</p>
       </div>
     </>
   );
